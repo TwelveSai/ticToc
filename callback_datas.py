fa78d9cfa36310152e18d8de9c3c88784b5bb443
TwelveSai <85743494+TwelveSai@users.noreply.github.com>
2022-08-14 20:06:41 +0300

    Add files via upload

--- callback_datas.py
+++ callback_datas.py
@@ -1,27 +1,27 @@
-class Mydialog(StatesGroup):
-    otvet = State()
-
-
-@dp.message_handler()
-async def cmd_dialog(message: types.Message):
-    await Mydialog.otvet.set()
-
-    await message.reply("Человечишка, напиши мне свое жалкое мнение")
-
-
-    
-@dp.message_handler()
-async def send_welcome(message: types.Message):
-
-    await message.reply('Устанавливаю связь с сервером...')
-
-
-
-
-    
-def check_sub_channel(chat_member):
-   
-    if chat_member['status'] != 'left':
-        return True
-    else:
-        return False
+# class Mydialog(StatesGroup):
+#     otvet = State()
+#
+#
+# @dp.message_handler()
+# async def cmd_dialog(message: types.Message):
+#     await Mydialog.otvet.set()
+#
+#     await message.reply("Человечишка, напиши мне свое жалкое мнение")
+#
+#
+#
+# @dp.message_handler()
+# async def send_welcome(message: types.Message):
+#
+#     await message.reply('Устанавливаю связь с сервером...')
+#
+#
+#
+#
+#
+# def check_sub_channel(chat_member):
+#
+#     if chat_member['status'] != 'left':
+#         return True
+#     else:
+#         return False
